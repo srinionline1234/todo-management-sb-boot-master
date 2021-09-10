f47d176d6934e6ea351019e913c8ab943de3afe3
unknown <srini.online1234@gmail.com>
2021-09-10 23:13:11 +0530

    my first project

---
+++ src/test/java/net/guides/springboot/todomanagementspringboot/TodoManagementSpringBoot2ApplicationTests.java
@@ -0,0 +1,16 @@
+package net.guides.springboot.todomanagementspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+public class TodoManagementSpringBoot2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

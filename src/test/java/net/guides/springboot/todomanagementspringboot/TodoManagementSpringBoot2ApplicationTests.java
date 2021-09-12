f8de0a436fcf0075aa989a9a8fbc575acf9cff5e
srinionline1234 <srini.online1234@gmail.com>
2021-09-13 01:01:20 +0530

    build fail test

--- src/test/java/net/guides/springboot/todomanagementspringboot/TodoManagementSpringBoot2ApplicationTests.java
+++ src/test/java/net/guides/springboot/todomanagementspringboot/TodoManagementSpringBoot2ApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class TodoManagementSpringBoot2ApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public vouid contextLoads() {
 	}
 
 }
